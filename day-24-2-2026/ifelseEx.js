7543724711143244bffe424e89e17c4145607f9c
poojadubey111 <pooja.dubey@nimapinfotech.com>
2026-02-24 11:50:46 +0530

    implement if else example

---
+++ day-24-2-2026/ifelseEx.js
@@ -0,0 +1,15 @@
+const pet = "dog";
+
+if (pet === "lizard") {
+  console.log("I own a lizard");
+} else if (pet === "dog") {
+  console.log("I own a dog");
+} else if (pet === "cat") {
+  console.log("I own a cat");
+} else if (pet === "snake") {
+  console.log("I own a snake");
+} else if (pet === "parrot") {
+  console.log("I own a parrot");
+} else {
+  console.log("I don't own a pet");
+}
