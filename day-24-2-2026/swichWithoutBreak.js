632faf62cecbe807e52511d7cadeaec9d1101762
poojadubey111 <pooja.dubey@nimapinfotech.com>
2026-02-24 17:00:19 +0530

    Switch program without Break

---
+++ day-24-2-2026/swichWithoutBreak.js
@@ -0,0 +1,10 @@
+switch (2) {
+  case 1:
+    console.log("Number 1 was chosen");
+  case 2:
+    console.log("Number 2 was chosen");
+  case 3:
+    console.log("Number 3 was chosen");
+  default:
+    console.log("No number was chosen");
+}
